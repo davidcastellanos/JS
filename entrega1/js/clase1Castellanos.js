e1450b8039b9c9970f1c664d1f4e382a9993681d
davidcastellanos <castellanos.johann@gmail.com>
2021-09-15 10:27:12 -0500

    [UPDATE] Added new js and html files

---
+++ entrega1/js/clase1Castellanos.js
@@ -0,0 +1,14 @@
+let nombre = prompt('Ingresa tu nombre');
+let numero = parseInt(prompt('Ingresa un número para sumar'));
+let numeroDefault = 7;
+let texto1 = prompt('Ingresa un texto corto');
+let texto2 = prompt('Ingresa otro texto para formar una frase con el texto anterior')
+
+
+
+
+console.log(`Hola ${nombre}`);
+
+console.log(`Tu número ingresado se suma con el número ${numeroDefault}, El resultado de la suma es: ${numeroDefault + numero}`);
+
+alert(`${texto1} ${texto2}`)
