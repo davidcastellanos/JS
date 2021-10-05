0bcbdf49f8cf90ecbeca2a1ad2a9531eea5a53f9
davidcastellanos <castellanos.johann@gmail.com>
2021-10-05 10:01:59 -0500

    [UPDATE] Added class and constructor

--- simuladorInteractivo/assets/data.js
+++ simuladorInteractivo/assets/data.js
@@ -1,22 +1,40 @@
-export let datosEstudiantes = [
-  {
-    nombre: 'David',
-    parcial1: 0,
-    parcial2: 0,
-    examenFinal: 0,
-  },
+class CrearEstudiante {
+  constructor(nombre, parcial1, parcial2, examenFinal) {
+    this.nombre = nombre;
+    this.parcial1 = parcial1;
+    this.parcial2 = parcial2;
+    this.examenFinal = examenFinal;
+  }
+}
 
-  {
-    nombre: 'Pedro',
-    parcial1: 0,
-    parcial2: 0,
-    examenFinal: 0,
-  },
+let estudiante1 = new CrearEstudiante('David', 0, 0, 0);
+//console.log([estudiante1]);
+let estudiante2 = new CrearEstudiante('Pedro', 0, 0, 0);
+let estudiante3 = new CrearEstudiante('Juan', 0, 0, 0);
 
-  {
-    nombre: 'Juan',
-    parcial1: 0,
-    parcial2: 0,
-    examenFinal: 0,
-  },
-];
+export let datosEstudiantes = [estudiante1, estudiante2, estudiante3];
+
+//console.log(datosEstudiantes);
+
+// export let datosEstudiantes = [
+//   {
+//     nombre: 'David',
+//     parcial1: 0,
+//     parcial2: 0,
+//     examenFinal: 0,
+//   },
+
+//   {
+//     nombre: 'Pedro',
+//     parcial1: 0,
+//     parcial2: 0,
+//     examenFinal: 0,
+//   },
+
+//   {
+//     nombre: 'Juan',
+//     parcial1: 0,
+//     parcial2: 0,
+//     examenFinal: 0,
+//   },
+// ];
